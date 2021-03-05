f177335dd2f8b40e299224c90f7b65193ac5e066
Benjamin Villanueva <villanub@gmail.com>
2021-03-05 09:04:37 -0600

    add role assignment

--- TimerWorkbook/run.ps1
+++ TimerWorkbook/run.ps1
@@ -15,5 +15,16 @@ Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
 Write-Host "Github Action"
 $RG = New-AzResourceGroup -Name ben-func-work3 -Location "South Central US" -Force
 
-Write-Host $RG
+Invoke-webrequest -uri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-functions-managed-identity/azuredeploy.json -OutFile "$env:temp\template.json"
+
+$template = "$env:temp\template.json"
+
+#$RG = New-AzResourceGroup -Name ben-func-work2 -Location "South Central US" -Force
+
+$principal = New-AzResourceGroupDeployment -Name Func-Work -ResourceGroupName ben-func-work3 -TemplateFile $template  
+
+$Context = Get-AzContext
+
+New-AzRoleAssignment -ObjectId "$($principal.Outputs.Values.value)" -RoleDefinitionName Contributor -Scope "/subscriptions/$($Context.Subscription)"
+
 
